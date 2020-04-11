3560d506fb529c53ee182ed426fe4ad824cc3374
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-11 12:01:27 -0700

    added get office number function to manager class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -3,20 +3,21 @@ const Employee = require("./Employee")
 
 class Manager extends Employee {
     constructor(name,id,email,office) {
-
         super(name, id, email);
         this.name = name;
         this.id = id;
         this.email = email;
-        this.office =office;
+        this.office = office;
         this.role = "Manager";
 
 
 
     }
+
+    getOfficeNumber(){
+        return this.office
+    }
 }
 
-var marc = new Manager("Ana",1,"email",100)
-console.log(marc)
 
 module.exports = Manager;
