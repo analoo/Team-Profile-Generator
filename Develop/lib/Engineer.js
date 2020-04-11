5310bc799f0b08602f3d832e318d5a130c8fdd76
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-11 11:39:14 -0700

    added engineer class, unherited from employee.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Engineer extends Employee {
+    constructor(name,id,email,github) {
+        const title = "Engineer";
+
+        super(name, id, email, title);
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.github = github;
+
+
+    }
+}
+
+
+var marc = new Engineer("Ana",1,"email","analoo")
+console.log(marc)
